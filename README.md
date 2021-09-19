77c2689eedc1cda723070c731c527d3fe572d1fa
kayubakar0 <suryaadrian970@gmail.com>
2021-09-19 14:46:22 +0700

    Tower Defence

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # 2D_TowerDefence
+
+Nama : Muhammad Adrian Surya Saputra
+Id Agate : 389
---
+++ Tower Defence/Assets/Script/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void play()
+    {
+        Application.LoadLevel("Gameplay");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void backtomenu()
+    {
+        Application.LoadLevel("Main menu");
+    }
+}
